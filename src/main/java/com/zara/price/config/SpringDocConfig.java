c1b69d56ac389388a58c2baae5caa7ef8fe67c91
matmartin <matias.martin@mercadolibre.com>
2021-10-08 19:28:53 -0300

    config spring doc

---
+++ src/main/java/com/zara/price/config/SpringDocConfig.java
@@ -0,0 +1,22 @@
+package com.zara.price.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app.title}") String appName, @Value("${app.description}") String description, @Value("${app.version}") String version) {
+        return new OpenAPI().info(new Info().title(appName)
+                .version(version)
+                .description(description)
+                .contact(new Contact().name("price-api")
+                        .email("matiass.lean@gmail.com")));
+    }
+}
