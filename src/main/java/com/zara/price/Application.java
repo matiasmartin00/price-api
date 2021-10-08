6a4a24377283fa1e5837c6150c6d54befc5311da
matmartin <matias.martin@mercadolibre.com>
2021-10-08 19:02:37 -0300

    rename PriceApplication.java -> Application.java

--- src/main/java/com/zara/price/Application.java
+++ src/main/java/com/zara/price/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PriceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(PriceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
