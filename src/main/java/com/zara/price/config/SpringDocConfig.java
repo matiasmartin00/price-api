1cd2709048f1ddb72949534e5860176f4935dbcc
matmartin <matias.martin@mercadolibre.com>
2021-10-08 19:32:59 -0300

    fixes spring doc config

--- src/main/java/com/zara/price/config/SpringDocConfig.java
+++ src/main/java/com/zara/price/config/SpringDocConfig.java
@@ -3,17 +3,23 @@ package com.zara.price.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "app")
 public class SpringDocConfig {
 
+    private String title;
+    private String description;
+    private String version;
+
     @Bean
-    public OpenAPI customOpenAPI(@Value("${app.title}") String appName, @Value("${app.description}") String description, @Value("${app.version}") String version) {
-        return new OpenAPI().info(new Info().title(appName)
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title(title)
                 .version(version)
                 .description(description)
                 .contact(new Contact().name("price-api")
